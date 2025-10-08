9f134903ca4037880dd2dbdb4dcd1548b6cc89e9
ASPJava2023 <ankit.singh.pawar.newlaptop@gmail.com>
2025-10-08 18:50:42 +0530

    Deleted old files from main

--- src/main/java/topic1Operators/P3.java
+++
@@ -1,9 +0,0 @@
-package topic1Operators;
-
-public class P3 {
-    public static void main(String[] args) {
-        int age = 17;
-        String result = (age>=18) ?"Yes":"No";
-        System.out.println("Is the person an adult? " + result);
-    }
-}
